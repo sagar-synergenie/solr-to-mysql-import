2fc9945fffffd44343fbcaf95cdbda95a4575797
Sagar Acharya <sagar_acharya@live.in>
2018-05-12 00:18:20 +0530

    table name to model

--- app/MissingHackSourceRecord.php
+++ app/MissingHackSourceRecord.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class MissingHackSourceRecord extends Model
 {
+    protected $table = 'missing_hack_source_records';
+    
     protected $fillable = ['email','sourceid','recordid'];
 }
